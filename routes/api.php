250133788b332c9bea1f2347f115d68eafacfd4c
Ricard Barnes <ricardbarnes@gmail.com>
2020-11-30 15:20:05 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\GetController;
+use App\Http\Controllers\JsonController;
+use App\Http\Controllers\PocahontasController;
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
